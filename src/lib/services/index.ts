61158657d5796357ce24a271a3f929078b68d920
ExecutiveDirector <105132421+ExecutiveDirector@users.noreply.github.com>
2026-01-02 19:01:02 +0300

    Create index.ts

---
+++ src/lib/services/index.ts
@@ -0,0 +1,11 @@
+
+// ============================================================
+// FILE: src/lib/services/index.ts
+// Export all services from a single file
+// ============================================================
+
+export { default as authService } from './authService';
+export { default as productService } from './productService';
+export { default as orderService } from './orderService';
+export { default as outletService } from './outletService';
+export { default as walletService } from './walletService'
