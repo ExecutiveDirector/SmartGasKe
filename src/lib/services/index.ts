6de9780013823e13ebb15b29565631c73ebea318
ExecutiveDirector <105132421+ExecutiveDirector@users.noreply.github.com>
2026-01-03 07:20:29 +0300

    Update index.ts

--- src/lib/services/index.ts
+++ src/lib/services/index.ts
@@ -8,4 +8,4 @@ export { default as authService } from './authService';
 export { default as productService } from './productService';
 export { default as orderService } from './orderService';
 export { default as outletService } from './outletService';
-export { default as walletService } from './walletService'
+export { default as walletService } from './walletService';
