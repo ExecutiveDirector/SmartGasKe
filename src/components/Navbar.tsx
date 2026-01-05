5ceb9d55778471111263e460d49c006c9ecf5e50
ExecutiveDirector <105132421+ExecutiveDirector@users.noreply.github.com>
2026-01-05 18:17:56 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,23 @@
-// src/components/Navbar.tsx
-import Link from 'next/link';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <span className="font-bold text-xl cursor-pointer">AquaGas</span>
+        <Link to="/" className="font-bold text-xl">
+          AquaGas
         </Link>
+
         <div className="flex gap-6">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/founder">Founder</Link>
-          <Link href="/how-it-works">How It Works</Link>
-          <Link href="/technology">Technology</Link>
-          <Link href="/investors">Investors</Link>
-          <Link href="/partners">Partners</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/shop">Shop</Link>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/founder">Founder</Link>
+          <Link to="/how-it-works">How It Works</Link>
+          <Link to="/technology">Technology</Link>
+          <Link to="/investors">Investors</Link>
+          <Link to="/partners">Partners</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/shop">Shop</Link>
         </div>
       </div>
     </nav>
