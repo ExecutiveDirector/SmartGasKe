c895b0427753e660eb1bb9c0cafec4af4243270c
ExecutiveDirector <105132421+ExecutiveDirector@users.noreply.github.com>
2026-01-09 17:52:44 +0300

    Update index.tsx

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Filter, ChevronRight, Loader } from 'lucide-react';
+import { ChevronRight, Loader } from 'lucide-react';
 import ProductCard from '@/components/ProductCard';
 import VendorCard from '@/components/VendorCard';
 import Carousel from '@/components/Carousel';
